dca9f9b6de11308d097eff3059acc8cd630a23f6
EPS OKC <epsokc@ip-192-168-28-17.ec2.internal>
2016-03-02 10:42:06 -0600

    testing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/deploy', function(req, res){
+app.post('/deploy', function(req, res){
     console.log(req);
     console.log('deploy');
     console.log(res);
