5f2fe82dac5b2fa22fc95c2f1535db3a8023e3e5
EPS OKC <epsokc@ip-192-168-28-17.ec2.internal>
2016-03-02 11:25:36 -0600

    host

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ require('./global.js');
 var githubhook = require('githubhook');
 var github = githubhook({
     secret: process.env.GITHUB_DEPLOY_SECRET,
-    path: '/deploy'
+    path: '/deploy',
+    host: '*'
 });
 
 github.listen();
